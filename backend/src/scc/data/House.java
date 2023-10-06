49e4b558245d74d6bca1967e953152e3d424641b
Pedro Manuel Ribeiro <pmd.ribeiro@campus.fct.unl.pt>
2023-10-06 13:36:31 +0100

    Added Rest endpoints for house and Media
    Added only the endpoints the functions still need to be developed

---
+++ backend/src/scc/data/House.java
@@ -0,0 +1,53 @@
+package scc.data;
+import org.apache.commons.codec.digest.DigestUtils;
+import java.util.Arrays;
+
+/**
+ * Represents a User, as returned to the clients
+ * 
+ * NOTE: array of house ids is shown as an example of how to store a list of elements and 
+ * handle the empty list.
+ */
+public class House {
+	private String id;
+	private String name;
+	private String description;
+	private String[] photoIds;
+	public User(String id, String name, String description,  String[] photoIds) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.houseIds = houseIds;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getdescription() {
+		return description;
+	}
+	public void setdescription(String description) {
+		this.pwd =description;
+	}
+	public String[] getphotoIds() {
+		return photoIds == null ? new String[0] : photoIds ;
+	}
+	public void setphotoIds(String[] photoIds) {
+		this.photoIds = photoIds;
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", Description=" + description +  ", photoIds="
+				+ Arrays.toString(photoIds) + "]";
+	}
+}
+
---
+++ backend/src/scc/srv/Mainapplication.java
@@ -0,0 +1,26 @@
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.ws.rs.core.Application;
+
+public class MainApplication extends Application
+{
+	private Set<Object> singletons = new HashSet<Object>();
+	private Set<Class<?>> resources = new HashSet<Class<?>>();
+
+	public MainApplication() {
+		resources.add(ControlResource.class);
+		singletons.add( new MediaResource());	
+	}
+
+	@Override
+	public Set<Class<?>> getClasses() {
+		return resources;
+	}
+
+	@Override
+	public Set<Object> getSingletons() {
+		return singletons;
+	}
+}
