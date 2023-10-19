33f44f0afd413bd63a4ae2ae0eb51c3a33f0cc6f
Kacper Motyka <kmotyka@student.agh.edu.pl>
2023-10-20 00:13:14 +0100

    House and User container - to improve

--- backend/src/main/java/scc/db/blob/HouseContainer.java
+++ backend/src/main/java/scc/db/blob/HouseContainer.java
@@ -1,5 +1,7 @@
 package scc.db.blob;
 
+import com.azure.core.util.BinaryData;
+import com.azure.storage.blob.BlobClient;
 import com.azure.storage.blob.BlobContainerClient;
 
 public class HouseContainer extends BlobContainer {
@@ -7,5 +9,23 @@ public class HouseContainer extends BlobContainer {
     HouseContainer(BlobContainerClient container) {
         super(container);
     }
-    
+
+    // TODO: What to return?
+    public String uploadImage(String id, byte[] image) {
+        
+        BlobClient blobClient = container.getBlobClient(id);
+        BinaryData binaryImage = BinaryData.fromBytes(image);
+        blobClient.upload(binaryImage);
+
+        return "File uploaded successfully ";
+    }
+
+
+    public byte[] getImage(String id) {
+        BlobClient blobClient = container.getBlobClient(id);
+
+        BinaryData image = blobClient.downloadContent();
+
+        return image.toBytes();
+    }
 }
--- backend/src/main/java/scc/db/blob/UserContainer.java
+++ backend/src/main/java/scc/db/blob/UserContainer.java
@@ -1,11 +1,35 @@
 package scc.db.blob;
 
+import com.azure.core.util.BinaryData;
+import com.azure.storage.blob.BlobClient;
 import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.models.BlobItem;
+import com.azure.storage.blob.models.BlobProperties;
+
+// Import BLOB equivalent to import com.azure.cosmos.models.CosmosItemResponse;
 
 public class UserContainer extends BlobContainer {
 
     UserContainer(BlobContainerClient container) {
         super(container);
+    }
+
+    // TODO: What to return?
+    public String uploadImage(String id, byte[] image) {
+        
+        BlobClient blobClient = container.getBlobClient(id);
+        BinaryData binaryImage = BinaryData.fromBytes(image);
+        blobClient.upload(binaryImage);
+
+        return "File uploaded successfully ";
+    }
+
+ 
+    public byte[] getImage(String id) {
+        
+        BlobClient blobClient = container.getBlobClient(id);
+        BinaryData image = blobClient.downloadContent();
+        return image.toBytes();
     }
     
 }
