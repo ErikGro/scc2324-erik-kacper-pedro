eda4bbb9a3bd2c4c079cd83b265d606d5cc13dae
Kacper Motyka <kmotyka@student.agh.edu.pl>
2023-10-21 15:07:52 +0100

    User - Upload and Download a photo

--- backend/src/main/java/scc/db/blob/UserContainer.java
+++ backend/src/main/java/scc/db/blob/UserContainer.java
@@ -1,8 +1,11 @@
 package scc.db.blob;
 
+import java.io.ByteArrayInputStream;
+
 import com.azure.core.util.BinaryData;
 import com.azure.storage.blob.BlobClient;
 import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.models.BlobHttpHeaders;
 import com.azure.storage.blob.models.BlobItem;
 import com.azure.storage.blob.models.BlobProperties;
 
@@ -18,18 +21,19 @@ public class UserContainer extends BlobContainer {
     public String uploadImage(String id, byte[] image) {
         
         BlobClient blobClient = container.getBlobClient(id);
-        BinaryData binaryImage = BinaryData.fromBytes(image);
-        blobClient.upload(binaryImage);
+        
+        blobClient.upload(new ByteArrayInputStream(image), image.length, true);
+
+        BlobHttpHeaders headers = new BlobHttpHeaders().setContentType("image/jpeg");
+        blobClient.setHttpHeaders(headers);
 
         return "File uploaded successfully ";
     }
 
- 
+
     public byte[] getImage(String id) {
-        
         BlobClient blobClient = container.getBlobClient(id);
         BinaryData image = blobClient.downloadContent();
         return image.toBytes();
     }
-    
 }
